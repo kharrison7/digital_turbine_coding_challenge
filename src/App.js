74c2f949d5b1ad12c9c6d246555a1f7b5ae8680e
Kevin Harrison <kharrison7@elon.edu>
2017-11-08 17:28:07 -0500

    This updates the readme and prepares the code to display on the page.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ class App extends Component {
 
     this.state = {
       example: '',
+      url: '',
       json: {},
     };
   }
 
+  // These are in case the application needs to handle inputs.
   handleCommentTextChange(event){
     this.setState({
       example: event.target.value
@@ -23,9 +25,12 @@ class App extends Component {
     event.preventDefault()
   }
 
+  // This loads the data when the page is mounted.
   componentWillMount() {
+    // This code generates the url to make the request.
     let url = 'http://ads.appia.com/getAdsDebug?id=3855&password=OCD8KOQI8F4MPUPXDXDE0V4EZF&key=r3c@che4t0mc@t&userAgentHeader=Android&siteId=9814';
     let paramName = 'sessionid';
+    // This generates a random number 1-10 inclusive.
     let value = Math.floor((Math.random() * 10) + 1);
     let addToEnd = '&sessionId=' + value;
     // The following code is optional to avoid CORS.
@@ -42,39 +47,19 @@ class App extends Component {
               if (err) {
                 console.log("failed to GET");
                 console.log(err);
-                // console.log(res);
               } else {
                 console.log(res.body);
-                this.setState({json: res.body}, () => {
+                this.setState({json: res.body, url: urlSearch}, () => {
                 });
               }
           })
-
-          // This seems to be a valid url:
-          // http://ads.appia.com/getAdsDebug?id=3855&password=OCD8KOQI8F4MPUPXDXDE0V4EZF&key=r3c@che4t0mc@t&userAgentHeader=Android&siteId=9814&sessionId=5
-
-          // fetch(urlSearch)
-          // .then(r => r.json() )
-          // .then((json) => {
-          //   console.log("Data from componentWillMount fetch", json)
-          //   this.setState({json: json})
-          // })
-
-          // $.ajax({
-          //   url: urlSearch,
-          //   dataType: 'json',
-          //   success: function( data ) {
-          //     console.log( "Success:", data);
-          //   },
-          //   error: function( data ) {
-          //     console.log( "Error:", data);
-          //   }
-          // });
   }
 
   render() {
     let json = this.state.json;
+    let url = this.state.url;
     console.log(json);
+    console.log(url);
     return (
       <div className="App container-fluid">
         <h1>Here is some text</h1>
