5fefbef348534643c4e724f82d381a8269b40c24
Kevin Harrison <kharrison7@elon.edu>
2017-11-08 17:19:30 -0500

    This returns a response with CORs on.

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ class App extends Component {
     })
   }
 
-
-
   handleFormSubmit(event){
     event.preventDefault()
   }
@@ -28,9 +26,13 @@ class App extends Component {
   componentWillMount() {
     let url = 'http://ads.appia.com/getAdsDebug?id=3855&password=OCD8KOQI8F4MPUPXDXDE0V4EZF&key=r3c@che4t0mc@t&userAgentHeader=Android&siteId=9814';
     let paramName = 'sessionid';
-    let value = 1;
-    let addToEnd = '&sessionid=1';
+    let value = Math.floor((Math.random() * 10) + 1);
+    let addToEnd = '&sessionId=' + value;
+    // The following code is optional to avoid CORS.
+    // const proxyurl = "https://boiling-castle-73930.herokuapp.com/";
+    // let urlSearch = `${proxyurl}` + url + addToEnd;
     let urlSearch = url + addToEnd;
+    console.log(value);
     console.log(urlSearch);
 
     // This makes the GET request.
@@ -40,18 +42,34 @@ class App extends Component {
               if (err) {
                 console.log("failed to GET");
                 console.log(err);
+                // console.log(res);
               } else {
+                console.log(res.body);
                 this.setState({json: res.body}, () => {
                 });
               }
           })
 
-          // fetch('http://ads.appia.com/getAdsDebug?id=3855&password=OCD8KOQI8F4MPUPXDXDE0V4EZF&key=r3c@che4t0mc@t&userAgentHeader=Android&siteId=9814')
+          // This seems to be a valid url:
+          // http://ads.appia.com/getAdsDebug?id=3855&password=OCD8KOQI8F4MPUPXDXDE0V4EZF&key=r3c@che4t0mc@t&userAgentHeader=Android&siteId=9814&sessionId=5
+
+          // fetch(urlSearch)
           // .then(r => r.json() )
           // .then((json) => {
           //   console.log("Data from componentWillMount fetch", json)
           //   this.setState({json: json})
           // })
+
+          // $.ajax({
+          //   url: urlSearch,
+          //   dataType: 'json',
+          //   success: function( data ) {
+          //     console.log( "Success:", data);
+          //   },
+          //   error: function( data ) {
+          //     console.log( "Error:", data);
+          //   }
+          // });
   }
 
   render() {
