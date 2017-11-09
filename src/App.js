1c612df916650b0477afcb2833cf4e88316925a3
Kevin Harrison <kharrison7@elon.edu>
2017-11-08 19:11:33 -0500

    This completes styling for the table.

--- src/App.js
+++ src/App.js
@@ -6,35 +6,22 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      example: '',
       url: '',
       intValue: 0,
       json: {},
     };
   }
 
-  // These are in case the application needs to handle inputs.
-  handleCommentTextChange(event){
-    this.setState({
-      example: event.target.value
-    })
-  }
-
-  handleFormSubmit(event){
-    event.preventDefault()
-  }
-
   // This loads the data when the page is mounted.
   componentWillMount() {
     // This code generates the url to make the request.
     let url = 'http://ads.appia.com/getAdsDebug?id=3855&password=OCD8KOQI8F4MPUPXDXDE0V4EZF&key=r3c@che4t0mc@t&userAgentHeader=Android&siteId=9814';
-    let paramName = 'sessionid';
+    let paramName = 'sessionId';
     // This generates a random number 1-10 inclusive.
     let value = Math.floor((Math.random() * 10) + 1);
-    let addToEnd = '&sessionId=' + value;
-    // The following code is optional to avoid CORS.
+    let addToEnd = `&${paramName}=` + value;
+    // The following code is optional to avoid the need for CORS.
     const proxyurl = "https://boiling-castle-73930.herokuapp.com/";
     let urlSearch = `${proxyurl}` + url + addToEnd;
     // let urlSearch = url + addToEnd;
@@ -50,7 +37,7 @@ class App extends Component {
                 console.log("failed to GET");
                 console.log(err);
               } else {
-                // console.log(res.body);
+                // This sets the state to fit the response.
                 this.setState({json: res.body, url: urlSearch}, () => {
                 });
               }
@@ -91,8 +78,8 @@ class App extends Component {
                     return(
                       <tr key={i} >
                         <td>{term.campaignId}</td>
-                        <td>{term.campaignName}</td>
-                        <td>{term.failedFilters[0].message}</td>
+                        <td className="align-left">{term.campaignName}</td>
+                        <td className="align-left">{term.failedFilters[0].message}</td>
                       </tr>
                     )
                   })}
